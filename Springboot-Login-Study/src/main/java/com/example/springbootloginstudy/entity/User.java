6094014697791bb96525fd8d69cbe4510900ef3a
김효건 <gyrjs0807@gmail.com>
2024-03-07 00:05:44 +0900

    refactor : @RequiredArgsConstructor -> @AllArgsConstructor 변경

--- Springboot-Login-Study/src/main/java/com/example/springbootloginstudy/entity/User.java
+++ Springboot-Login-Study/src/main/java/com/example/springbootloginstudy/entity/User.java
@@ -6,15 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	@Id
