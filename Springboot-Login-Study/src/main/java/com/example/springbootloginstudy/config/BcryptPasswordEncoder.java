cae6198dc5c46b87df943759b33870c36fe33987
HG <gyrjs0807@gmail.com>
2024-03-11 23:48:29 +0900

    feat : BcryptPasswordEncoder, SecurityConfig 추가

---
+++ Springboot-Login-Study/src/main/java/com/example/springbootloginstudy/config/BcryptPasswordEncoder.java
@@ -0,0 +1,13 @@
+package com.example.springbootloginstudy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BcryptPasswordEncoder {
+
+	@Bean
+	public BcryptPasswordEncoder passwordEncoder() {
+		return new BcryptPasswordEncoder();
+	}
+}
---
+++ Springboot-Login-Study/src/main/java/com/example/springbootloginstudy/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.springbootloginstudy.config;
+
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.springbootloginstudy.domain.UserRole;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeRequests()
+			.requestMatchers("/security-login/info").authenticated()
+			.requestMatchers("/security-login/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
+			.anyRequest().permitAll()
+			.and()
+			.formLogin(
+				formLogin ->
+					formLogin
+						.usernameParameter("loginId")
+						.passwordParameter("password")
+						.loginPage("/security-login/login")
+						.defaultSuccessUrl("/security-login")
+						.failureUrl("/security-login/login")
+			)
+
+			.logout((logout) -> logout
+				.logoutSuccessUrl("/security-login/logout")
+				.invalidateHttpSession(true).deleteCookies());
+
+		return http.build();
+	}
+}
+
+// antMatcher -> requestMatcher
